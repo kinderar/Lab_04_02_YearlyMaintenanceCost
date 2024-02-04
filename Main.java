a33cca3bdc3dac83bd6fe930495a4ec0caeeda16
kinderar <156221770+kinderar@users.noreply.github.com>
2024-02-04 16:25:32 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int springCost = 2500; //input replacement
+        int summerCost = 3500; //input replacement
+        int fallCost = 2500; //input replacement
+        int winterCost = 4000; //input replacement
+        int yearlyCost = 0;
+            yearlyCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("The total cost is " + yearlyCost + " with spring costing " + springCost + ", summer costing " + summerCost + ", fall costing " + fallCost + ", and winter costing " + winterCost + ".");
+    }
+}
